c9434fdbd83ef4fc108844b4a6a8d453fe0736b6
Isaac Svedin <swedinisaac@gmail.com>
2025-02-06 16:01:09 +0100

    Updates

---
+++ index.80b0072b.js
@@ -0,0 +1,28 @@
+!function(){let e=document.getElementById("prev-btn"),t=document.getElementById("next-btn"),a=document.getElementById("page-number"),l=document.getElementById("max-pages"),i=document.getElementById("anime-list");async function s(e=1){if(localStorage.getItem("page")){let e=JSON.parse(localStorage.getItem("page"));a.innerHTML=e.current,l.innerHTML=e.last}if(localStorage.getItem("animePagesofLists")){let a=JSON.parse(localStorage.getItem("animePagesofLists"));for(let l of a){if(l.page===e){var t;t=l.list,i.innerHTML="",t.forEach(e=>{let t=e.title_english;null===e.title_english&&(t=e.title),i.innerHTML+=`<article class=anime-list-item>
+                    <h2>${t}</h2>
+                    <div>
+                        <picture class=poster>
+                            <source srcset="${e.images.webp.image_url}" type="image/webp">
+                            <img src="${e.images.jpg.image_url}" width="100" height="141" alt="" loading="lazy" class=poster>
+                        </picture>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Score</th>
+                                    <th>Rating</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>#${e.rank}</td>
+                                    <td>${e.score}</td>
+                                    <td>${e.rating}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Beskrivning</h3>
+                        <p>${e.synopsis}</p>
+                    </div>
+                </article>`});break}a.indexOf(l)===a.length-1&&l.page!==e&&(await n(e),await s(e))}}}async function n(e=1){try{let t=await fetch(`https://api.jikan.moe/v4/top/anime?page=${e}&limit=10`);if(429===t.status){alert("Maximalt antal förfrågningar till API:n är begränsad, men alla föregående sidor som redan hämtas fungerar som normalt.");return}let a=await t.json();localStorage.setItem("page",JSON.stringify({current:e,last:a.pagination.last_visible_page})),r(e,a.data)}catch(e){console.error(e)}}async function r(e,t){if(localStorage.getItem("animePagesofLists")){let a=JSON.parse(localStorage.getItem("animePagesofLists"));a.push({page:e,list:t}),localStorage.setItem("animePagesofLists",JSON.stringify(a))}else localStorage.setItem("animePagesofLists",JSON.stringify([{page:e,list:t}]))}window.onload=async()=>{if(localStorage.getItem("animePagesofLists")||await n(),localStorage.getItem("page")){let e=JSON.parse(localStorage.getItem("page"));a.innerHTML=e.current,l.innerHTML=e.last}s()},e.addEventListener("click",()=>{if(localStorage.getItem("page")){let e=JSON.parse(localStorage.getItem("page")),t=(e.current-1+e.last)%e.last;0==t&&(t=e.last),localStorage.setItem("page",JSON.stringify({current:t,last:e.last})),s(t)}}),t.addEventListener("click",()=>{if(localStorage.getItem("page")){let e=JSON.parse(localStorage.getItem("page")),t=(e.current+1)%e.last;0==t&&(t=e.last),localStorage.setItem("page",JSON.stringify({current:t,last:e.last})),s(t)}})}();
+//# sourceMappingURL=index.80b0072b.js.map
--- index.b8fca702.js
+++ index.b8fca702.js
@@ -598,14 +598,30 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"3cYfC":[function(require,module,exports,__globalThis) {
 var _roboto = require("@fontsource/roboto"); // Normal
 var _700Css = require("@fontsource/roboto/700.css"); // Bold
+const prevBTN = document.getElementById("prev-btn");
+const nextBTN = document.getElementById("next-btn");
+const pageNumberSPAN = document.getElementById("page-number");
+const maxPageSPAN = document.getElementById("max-pages");
 const animeListDIV = document.getElementById("anime-list");
 // När sidan laddas in hämtas och renderas första sidan av anime listan.
 window.onload = async ()=>{
     if (!localStorage.getItem("animePagesofLists")) await getTopAnime();
+    // Sätter visuellt vilken sida som användaren står på vid inladdning av sidan.
+    if (localStorage.getItem("page")) {
+        const pageData = JSON.parse(localStorage.getItem("page"));
+        pageNumberSPAN.innerHTML = pageData.current;
+        maxPageSPAN.innerHTML = pageData.last;
+    }
     renderList();
 };
 // Renderar en sida beroende på vilken sida det är, standard är första sidan.
 async function renderList(page = 1) {
+    // Uppdaterar visuellt vilken sida användaren står på.
+    if (localStorage.getItem("page")) {
+        const pageData = JSON.parse(localStorage.getItem("page"));
+        pageNumberSPAN.innerHTML = pageData.current;
+        maxPageSPAN.innerHTML = pageData.last;
+    }
     if (localStorage.getItem("animePagesofLists")) {
         // Hämtar listan från localStorage:
         const pagesofLists = JSON.parse(localStorage.getItem("animePagesofLists"));
@@ -663,7 +679,15 @@ async function renderList(page = 1) {
 async function getTopAnime(page = 1) {
     try {
         const resp = await fetch(`https://api.jikan.moe/v4/top/anime?page=${page}&limit=10`);
+        if (resp.status === 429) {
+            alert("Maximalt antal f\xf6rfr\xe5gningar till API:n \xe4r begr\xe4nsad, men alla f\xf6reg\xe5ende sidor som redan h\xe4mtas fungerar som normalt.");
+            return;
+        }
         const data = await resp.json();
+        localStorage.setItem("page", JSON.stringify({
+            current: page,
+            last: data.pagination.last_visible_page
+        })); // Sätter/uppdaterar vilken sida användaren står på.
         animePagesofLists(page, data.data); // Anropar för att ordna en array som ska lagras i localStorage.
     } catch (error) {
         console.error(error);
@@ -687,6 +711,35 @@ async function animePagesofLists(page, animeList) {
         }
     ]));
 }
+// Till föregående sida.
+prevBTN.addEventListener("click", ()=>{
+    if (localStorage.getItem("page")) {
+        const pageData = JSON.parse(localStorage.getItem("page"));
+        // Stegar bakåt i en loop. Om användaren står på sida 1 blir föregående sida sista sidan med hjälp av if-satsen nedan.
+        let prevPage = (pageData.current - 1 + pageData.last) % pageData.last;
+        // Sista sidan blir pageData.last % pageData.last men det blir 0 och det finns inte och för att sista sidan ska bli möjlig måste sida 0 vara sista sidan. 
+        if (prevPage == 0) prevPage = pageData.last;
+        localStorage.setItem("page", JSON.stringify({
+            current: prevPage,
+            last: pageData.last
+        })); // Sätter/uppdaterar vilken sida användaren står på.
+        renderList(prevPage); // Renderar listan.
+    }
+});
+// Till nästa sida.
+nextBTN.addEventListener("click", ()=>{
+    if (localStorage.getItem("page")) {
+        const pageData = JSON.parse(localStorage.getItem("page"));
+        // Stegar frammåt i en loop. Precis som ovanstående funktion men åt andra hållet.
+        let nextPage = (pageData.current + 1) % pageData.last;
+        if (nextPage == 0) nextPage = pageData.last;
+        localStorage.setItem("page", JSON.stringify({
+            current: nextPage,
+            last: pageData.last
+        })); // Sätter/uppdaterar vilken sida användaren står på.
+        renderList(nextPage); // Renderar listan.
+    }
+});
 
 },{"@fontsource/roboto":"jgvpq","@fontsource/roboto/700.css":"4mrIr"}],"jgvpq":[function() {},{}],"4mrIr":[function() {},{}]},["k51Nd","3cYfC"], "3cYfC", "parcelRequire94c2")
 
