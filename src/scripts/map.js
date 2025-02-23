8c346643e2c9454595b7c14cf9d3a8fa1550863c
Isaac Svedin <swedinisaac@gmail.com>
2025-02-23 14:16:35 +0100

    Added show results.

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,12 +1,28 @@
 const searchPlaceINPUT = document.getElementById("search");
+const searchResultsDIV = document.getElementById("search-results");
 
 let timeout;
 searchPlaceINPUT.addEventListener("input", () => {
     clearTimeout(timeout);
     timeout = setTimeout(async () => {
         const places = await getPlacesFromSearch(searchPlaceINPUT.value.trim());
+        listSearchResults(places);
     }, 500);
 });
+
+/**
+ * Listar sökresultat.
+ * @param {En array av platser} places 
+ */
+function listSearchResults(places) {
+    searchResultsDIV.innerHTML = "";
+    places.forEach(place => {
+        const resultItemDIV = document.createElement("div");
+        resultItemDIV.className = "result-item";
+        resultItemDIV.innerHTML = `<p class=place-name><strong>${place.name}</strong></p><p class=coords>Latitud: ${place.lat} Longitud: ${place.lon}</p>`;
+        searchResultsDIV.appendChild(resultItemDIV);
+    });
+}
 
 /**
  * Hämtar platsers namn och koordinater.
@@ -26,7 +42,6 @@ async function getPlacesFromSearch(query) {
                 lon: d.lon
             });
         });
-        console.log(places);
         return places;
     } catch(error) {
         console.error(error);
